36582cefbc9e948c11959380da0bd19db813cb5d
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-04-13 16:53:38 +0100

    elaboration added

--- src/pages/Deliverables/index.js
+++ src/pages/Deliverables/index.js
@@ -6,10 +6,12 @@ const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 
 const Home = () => {
   const deliverables = [
-    { name: "Micro Site", version: "1.0", link: "/" },
+    { name: "Micro Site", version: "1.1", link: "/" },
     { name: "Apresentação Do Projeto", version: "1.0", link: "/" },
-    { name: "Centro de controlo de tráfego", version: "", link: "" },
+    { name: "Centro de controlo de tráfego", version: "0.1", link: "https://ccam.av.it.pt" },
     { name: "C-ITS Mobile App", version: "", link: "" },
+    { name: "Elaboration Relatório", version: "1.0", link: "/doc/PEI-Elaboration-Presentation.pdf" },
+    { name: "Elaboration Apresentação", version: "1.0", link: "/doc/PEI-Elaboration-Report.pdf" },
     { name: "Relatório Final", version: "", link: "" },
   ]
 
