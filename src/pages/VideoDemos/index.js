f251895e51f353673dd95ef96aabd0558797140b
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-04-28 15:03:37 +0100

    wrap working

--- src/pages/VideoDemos/index.js
+++ src/pages/VideoDemos/index.js
@@ -1,42 +1,46 @@
-import { Row } from "antd";
 import { lazy } from "react";
 import ReactPlayer from 'react-player'
 
-const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 
 const Home = () => {
   const videos = [
-    { title: "test", url: "video/test.mp4", key: 1 },
-    { title: "test", url: "video/test.mp4", key: 1 },
-    { title: "test", url: "video/test.mp4", key: 1 },
-    { title: "test", url: "video/test.mp4", key: 1 },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
+    { title: "test", url: "video/test.mp4" },
   ];
   return (
-    <Container>
+    <>
       <ScrollToTop />
-      {videos.map((video) => {
-        return (
-            <Container style={styles.centeredDiv} >
-              <Row style={styles.centeredDiv}>
-                <h1>{video.title}</h1>
-              </Row>
-              <Row style={styles.centeredDiv}>
-                <ReactPlayer url={video.url} controls={true} />
-              </Row>
-          </Container>
-        )
-      })}
-    </Container>
+      <div style={{ display: "flex", width: "100%", flexDirection: "row", flexWrap: "wrap" }}>
+        {videos.map((item, index) => {
+          return (
+            <div key={index} style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "50%",
+              alignItems: "center",
+              justifyContent: "center"
+            }}>
+              <h1>{item.title}</h1>
+              <ReactPlayer url={item.url} controls={true} />
+            </div>
+          )
+        })}
+      </div>
+    </>
   );
 };
 
 const styles = {
   centeredDiv: {
     display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap"
-
+    justifyContent: "center"
   }
 };
 
