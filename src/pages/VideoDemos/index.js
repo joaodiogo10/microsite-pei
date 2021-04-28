049611b0b361d6839ddf42e5551a27a564103d5e
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-04-28 22:12:46 +0100

    fixed version not working sorry

--- src/pages/VideoDemos/index.js
+++ src/pages/VideoDemos/index.js
@@ -1,14 +1,14 @@
 import { lazy } from "react";
 import ReactPlayer from 'react-player'
 
-const ScrollToTop = la  zy(() => import("../../common/ScrollToTop"));
+const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 
 const Home = () => {
   const videos = [
     { title: "Uma wild DENM aparece", url: "video/denm_appearing.mp4" },
     { title: "Auto-estrada com DENM", url: "video/highway_vith_denm.mp4" },
     { title: "Alterar o idioma", url: "video/laguage_switching.mp4" },
-    { title: "Enviar DENM no browser", url: "video/send__denm_pc.mp4" },
+    { title: "Enviar DENM no browser", url: "video/send__denm_pc.mp4" },];
   return (
     <>
       <div id="intro"></div>
