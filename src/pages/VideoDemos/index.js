9a4845161d1d440c69ef4424aeebf870abf71cf3
Gil Lopes Teixeira <beerkiller@sapo.pt>
2021-05-19 01:00:32 +0100

    added videos to the site

--- src/pages/VideoDemos/index.js
+++ src/pages/VideoDemos/index.js
@@ -5,10 +5,13 @@ const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
 
 const Home = () => {
   const videos = [
-    { title: "Uma DENM a aparecer", url: "video/denm_appearing.mp4" },
-    { title: "Auto-estrada com DENM", url: "video/highway_vith_denm.mp4" },
-    { title: "Alterar o idioma", url: "video/laguage_switching.mp4" },
-    { title: "Enviar DENM no browser", url: "video/send__denm_pc.mp4" },];
+    { title: "Uma DENM a aparecer (iPhone)", url: "video/iphone-denm-appearing.mp4" },
+    { title: "Auto-estrada com DENM (iPhone)", url: "video/iphone-highway-with-denm.mp4" },
+    { title: "Alterar o idioma (iPhone)", url: "video/iphone-laguage-switching.mp4" },
+    { title: "Enviar DENM (iPhone)", url: "video/iphone-send-denm.mp4" },
+    { title: "DENM enviada por um iPhone (Browser)", url: "video/browser-iphone-recv-denm.mp4" },
+    { title: "Enviar DENM (Browser)", url: "video/browser-send-denm.mp4" },
+  ];
   return (
     <>
       <div id="intro"></div>
@@ -23,8 +26,8 @@ const Home = () => {
               alignItems: "center",
               justifyContent: "center"
             }}>
-              <h1 style={{paddingTop: 100, textAlign: "center", paddingLeft: 5,  paddingRight: 5}}>{item.title}</h1>
-              <ReactPlayer url={item.url} controls={true} style={{paddingBottom: 40, maxHeight: 750}} width="75%" height="100%"/>
+              <h1 style={{ paddingTop: 100, textAlign: "center", paddingLeft: 5, paddingRight: 5 }}>{item.title}</h1>
+              <ReactPlayer url={item.url} controls={true} style={{ paddingBottom: 40, maxHeight: 750 }} width="75%" height="100%" />
             </div>
           )
         })}
